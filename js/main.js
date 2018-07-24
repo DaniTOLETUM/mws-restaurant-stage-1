cf42e961d42bf28743a9f2b4943f11d2f63ae5d9
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2018-07-24 11:50:20 +0200

    Google Maps Runs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let restaurants,
   neighborhoods,
   cuisines
-var newMap
+var map
 var markers = []
 
 /**
@@ -69,26 +69,9 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize leaflet map, called from HTML.
+ * Initialize GoogleMaps, called from HTML.
  */
-initMap = () => {
-  self.newMap = L.map('map', {
-        center: [40.722216, -73.987501],
-        zoom: 12,
-        scrollWheelZoom: false
-      });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(newMap);
-
-  updateRestaurants();
-}
-/* window.initMap = () => {
+window.initMap = () => {
   let loc = {
     lat: 40.722216,
     lng: -73.987501
@@ -98,8 +81,13 @@ initMap = () => {
     center: loc,
     scrollwheel: false
   });
+
+  google.maps.event.addDomListener(window, 'resize', function () {
+    map.setCenter(loc);
+  });
+
   updateRestaurants();
-} */
+}
 
 /**
  * Update page and map for current restaurants.
