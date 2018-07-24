e89a15728ce4880e9d86f1b23ef0bc6f7b70fc49
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2018-07-24 13:03:15 +0200

    API google maps reviewed

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -21,14 +21,14 @@ window.initMap = () => {
         zoom: 16,
         scrollWheelZoom: false
       });
-      /*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoiZGFuaWVsZGlheiIsImEiOiJjamp6a3ozcTAwYjExM3JwNWM5czR3b3diIn0.kpBxvr3kAzbVFHU9WThS7w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'mapbox.streets'    
-      }).addTo(newMap);*/
+      }).addTo(map);
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
       google.maps.event.addDomListener(window, 'resize', function (){
